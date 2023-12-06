8c7fbf57a4f15782d1fa2f4cd0576e1ced5b2b06
Adam Woodson <136396828+Acearoni@users.noreply.github.com>
2023-12-06 16:23:45 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1>Hello World</h1>
+    <h2>This is my new page</h2>
+    <ul>
+      <li>Tommy</li>
+      <li>Phil</li>
+      <li>Chucky</li>
+      <li>Lil</li>
+    </ul>
+    </div>
+  )
+}
+
+export default App
